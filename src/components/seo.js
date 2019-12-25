6695f87e0d98c34a425395f8572f59dcb6208460
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-12-25 23:50:21 +0700

    :mag: Setting SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
@@ -18,7 +11,8 @@ function SEO({ description, lang, meta, title }) {
           siteMetadata {
             title
             description
-            author
+            url
+            image
           }
         }
       }
@@ -36,8 +30,24 @@ function SEO({ description, lang, meta, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
+          name: `title`,
+          content: title,
+        },
+        {
           name: `description`,
           content: metaDescription,
+        },
+        {
+          name: `image`,
+          content: `${site.siteMetadata.url}${site.siteMetadata.image}`,
+        },
+        {
+          property: `type`,
+          content: `website`,
+        },
+        {
+          property: `url`,
+          content: site.siteMetadata.url,
         },
         {
           property: `og:title`,
@@ -48,24 +58,16 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
         {
+          property: `og:image`,
+          content:  `${site.siteMetadata.url}${site.siteMetadata.image}`,
+        },
+        {
           property: `og:type`,
           content: `website`,
         },
         {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
+          property: `og:url`,
+          content: site.siteMetadata.url,
         },
       ].concat(meta)}
     />
